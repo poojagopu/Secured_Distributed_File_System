c2a482d74a5f6b68f8458ddeb4926876ab7a1604
Ray Karyshyn <raykaryshyn@gmail.com>
2022-11-19 13:12:56 -0500

    server deleteFile operation

--- DFS_project/src/DistributedFileSystem.java
+++ DFS_project/src/DistributedFileSystem.java
@@ -4,9 +4,7 @@ import java.rmi.server.*;
 import java.util.*;
 
 public class DistributedFileSystem extends UnicastRemoteObject implements FileSystem {
-
-    DistributedFileSystem() throws RemoteException
-    {
+    DistributedFileSystem() throws RemoteException {
         super();
     }
 
@@ -16,14 +14,13 @@ public class DistributedFileSystem extends UnicastRemoteObject implements FileSy
      */
     @Override
     public String createFile(String fileName) throws IOException {
-
         File fileObject = new File(fileName);
         // Method createNewFile() method creates blank file
         if (fileObject.createNewFile()) {
             System.out.println("File created: " + fileObject.getName());
         } else {
-             System.out.println("File already exists.");
-             return "File already exists";
+            System.out.println("File already exists.");
+            return "File already exists";
         }
         return "File created successfully.";
     }
@@ -35,14 +32,15 @@ public class DistributedFileSystem extends UnicastRemoteObject implements FileSy
     @Override
     public String readFile(String filePath) throws IOException {
         String ans;
-        StringBuilder str=new StringBuilder();
+        StringBuilder str = new StringBuilder();
         // Creating an object of BufferedReader class
         try {
             BufferedReader br = new BufferedReader(new FileReader(filePath));
             while ((ans = br.readLine()) != null)
                 str.append(ans);
             System.out.println("Read successfully");
-        }catch (IOException e){
+            br.close();
+        } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
@@ -56,7 +54,7 @@ public class DistributedFileSystem extends UnicastRemoteObject implements FileSy
     @Override
     public String writeFile(String fileName, String data) throws RemoteException {
         try {
-            FileWriter fw=new FileWriter(fileName);
+            FileWriter fw = new FileWriter(fileName);
             BufferedWriter bw = new BufferedWriter(fw);
             System.out.println("Started writing");
             fw.write(data);
@@ -86,7 +84,20 @@ public class DistributedFileSystem extends UnicastRemoteObject implements FileSy
      */
     @Override
     public String deleteFile(String fileName) throws RemoteException {
+        String ans = "";
+        try {
+            File file = new File(fileName);
 
-        return null;
+            if (file.delete()) {
+                ans = "File deleted successfully";
+            } else {
+                ans = "Failed to delete the file";
+            }
+        } catch (SecurityException e) {
+            ans = "An error occurred.";
+            e.printStackTrace();
+        }
+        System.out.println(ans);
+        return ans;
     }
 }
--- DFS_project/src/FileSystem.java
+++ DFS_project/src/FileSystem.java
@@ -1,11 +1,16 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.rmi.*;
-public interface FileSystem extends Remote{
+
+public interface FileSystem extends Remote {
     // Declaring the method prototypes
     public String createFile(String fileName) throws IOException;
+
     public String readFile(String fileName) throws IOException;
+
     public String writeFile(String fileName, String data) throws RemoteException;
+
     public void restoreFiles(String fileName) throws RemoteException;
+
     public String deleteFile(String fileName) throws RemoteException;
 }
