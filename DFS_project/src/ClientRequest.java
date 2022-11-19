e0b02e6da6717b33e6f1e1b0044260b962d90302
Ray Karyshyn <raykaryshyn@gmail.com>
2022-11-19 13:13:24 -0500

    client shell environment

--- DFS_project/src/ClientRequest.java
+++ DFS_project/src/ClientRequest.java
@@ -1,34 +1,57 @@
-import java.io.File;
 import java.rmi.*;
 import java.util.*;
+
 public class ClientRequest {
-    public static void main(String args[])
-    {
-        String IP="10.0.0.245";
-        String port="1234";
-        String fileName="myFile.txt";
-        String createAnswer;
-        String writeAnswer;
-        String readAnswer;
-        try
-        {
+    public static void main(String args[]) {
+        String IP = "127.0.0.1";
+        String port = "1234";
+        String serverAnswer;
+        String userChoice;
+        String[] userLine;
+
+        try {
             // lookup method to find reference of remote object
-            FileSystem access = (FileSystem) Naming.lookup("rmi://"+IP+":"+port+"/usingRMI");
-            createAnswer= access.createFile(fileName);
-            Scanner scan=new Scanner(System.in);
-            System.out.println("Start Writing....");
-            String ans=scan.nextLine();
-            writeAnswer= access.writeFile(fileName,ans);
-            readAnswer= access.readFile(fileName);
-            System.out.println("createAnswer: "+createAnswer);
-            System.out.println("writeAnswer: "+writeAnswer);
-            System.out.println("readAnswer: "+readAnswer);
+            FileSystem access = (FileSystem) Naming.lookup("rmi://" + IP + ":" + port + "/usingRMI");
 
+            Scanner userScan = new Scanner(System.in);
+            help();
 
-        }
-        catch(Exception e)
-        {
+            do {
+                System.out.print("$ ");
+                userLine = userScan.nextLine().split(" ");
+                userChoice = userLine[0];
+
+                if (userChoice.equals("help")) {
+                    help();
+                } else if (userChoice.equals("create") && userLine.length > 1) {
+                    serverAnswer = access.createFile(userLine[1]);
+                    System.out.println(serverAnswer);
+                } else if (userChoice.equals("write") && userLine.length > 1) {
+                    System.out.println("Start Writing....");
+                    String ans = userScan.nextLine();
+                    serverAnswer = access.writeFile(userLine[1], ans);
+                    System.out.println(serverAnswer);
+                } else if (userChoice.equals("read") && userLine.length > 1) {
+                    serverAnswer = access.readFile(userLine[1]);
+                    System.out.println(serverAnswer);
+                } else if (userChoice.equals("delete") && userLine.length > 1) {
+                    serverAnswer = access.deleteFile(userLine[1]);
+                    System.out.println(serverAnswer);
+                } else if (!userChoice.equals("exit")) {
+                    System.out.println("Sorry, please enter valid command.");
+                }
+            } while (!userChoice.equals("exit"));
+
+            userScan.close();
+        } catch (Exception e) {
             System.out.println(e);
+            e.printStackTrace();
         }
+    }
+
+    private static void help() {
+        System.out.println("Write a command in the form of:");
+        System.out.println("  0-args >>> $ [help, exit]");
+        System.out.println("  1-arg  >>> $ [create, write, read, delete] filename");
     }
 }
